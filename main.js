e38b68eea97d7eb5d585c34491f1a5b20c09fc10
Henry Ripple <hripple46@gmail.com>
2023-01-25 18:20:23 -0600

    added background image and minor styling to cover page

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ let cryingWolf = document.querySelector("#cryingWolf");
 cryingWolf.addEventListener("click", () => {
   window.location.href = "collection.html";
 });
+
+//let about = document.querySelector("#")
