1a0d45bd7354903ef009071477738e1296c4bd3c
Henry Ripple <hripple46@gmail.com>
2023-01-26 19:26:30 -0600

    added full functionality to navbar - all spaces are clickable

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ let downAndAway = document.querySelector("#downAndAway");
 downAndAway.addEventListener("click", () => {
   window.location.href = "downAndAway.html";
 });
+
+let header_Name = document.querySelector("#name");
+header_Name.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
+
+let collection = document.querySelector("#collection1");
+collection.addEventListener("click", () => {
+  window.location.href = "collection.html";
+});
