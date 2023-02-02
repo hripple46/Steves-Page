f02ae2c794620d7cd7567f145fcc374d799175d1
Henry Ripple <hripple46@gmail.com>
2023-02-02 15:48:32 -0600

    api request pulls all posts about 'lost and away'

--- main.js
+++ main.js
@@ -23,13 +23,23 @@ collection.addEventListener("click", () => {
 
 //this fetches text updates for 'lostandaway'page
 fetch(
-  "https://cdn.contentful.com/spaces/qbdplr5aogh4/entries/2iBUtmIEsNoCOQsvQkyH4W?access_token=cj67-ZjOiTRefmEvkVqPvMFWX3RwPUXVwYWVQTKqCFE"
+  "https://cdn.contentful.com/spaces/qbdplr5aogh4/environments/master/entries?access_token=cj67-ZjOiTRefmEvkVqPvMFWX3RwPUXVwYWVQTKqCFE"
+  //"https://cdn.contentful.com/spaces/qbdplr5aogh4/entries/2iBUtmIEsNoCOQsvQkyH4W?access_token=cj67-ZjOiTRefmEvkVqPvMFWX3RwPUXVwYWVQTKqCFE"
 )
   .then(function (response) {
     return response.json();
   })
   .then(function (response) {
-    console.log(response.fields.lostandaway.content[0].content[0].value);
+    for (let keys in response) {
+      if (`${keys}` == "items") {
+        for (let i in response[keys]) {
+          console.log(response[keys][i]);
+          console.log(
+            response[keys][i].fields.lostandaway.content[0].content[0].value
+          );
+        }
+      }
+    }
   });
 
 //cj67-ZjOiTRefmEvkVqPvMFWX3RwPUXVwYWVQTKqCFE
