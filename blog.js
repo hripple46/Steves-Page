8604c9aad15638821d2084295bdfffc2dd27e100
Henry Ripple <hripple46@gmail.com>
2023-04-22 12:15:29 -0500

    fixed error pulling blog posts

--- blog.js
+++ blog.js
@@ -4,18 +4,23 @@ fetch(
   .then((response) => response.json())
   .then((data) => data.items)
   .then((data) => {
-    console.log(data);
     getBlogPosts(data);
   });
 
 let getBlogPosts = (data) => {
   for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
     if (data[i].sys.contentType.sys.id == "blogPost") {
-      console.log(data[i].fields);
-      let textEntry = data[i].fields.entry.content[0].content[0].value;
-      console.log("Text:" + textEntry);
-      let imageID = data[i].fields.image.sys.id;
-      console.log("Image ID:" + imageID);
+      let textNodes = data[i].fields.entry.content;
+      let textEntry = "";
+      textNodes.forEach((element) => {
+        console.log(element.content[0].value);
+        textEntry += element.content[0].value + "\n\n";
+      });
+
+      console.log("Image:");
+      console.log(data[i].sys.id);
+      let imageID = data[i].sys.id;
       getImages(textEntry, imageID);
     }
   }
@@ -25,13 +30,18 @@ async function getImages(text, id) {
     "https://cdn.contentful.com/spaces/qbdplr5aogh4/environments/master/assets/" +
       id +
       "?access_token=cj67-ZjOiTRefmEvkVqPvMFWX3RwPUXVwYWVQTKqCFE"
-  ).then((response) =>
-    response.json().then((data) => {
-      let image = data.fields.file.url;
-      console.log(data.fields.file.url);
-      postEntry(text, image);
-    })
-  );
+  )
+    .then((response) =>
+      response.json().then((data) => {
+        let image = data.fields.file.url;
+        console.log(data.fields.file.url);
+        postEntry(text, image);
+      })
+    )
+    .catch((error) => {
+      console.log(error);
+      postEntry(text, "");
+    });
 }
 let postEntry = (text, image) => {
   let blogPost = document.createElement("div");
@@ -42,6 +52,10 @@ let postEntry = (text, image) => {
   let blogImage = document.createElement("img");
   blogImage.src = image;
   blogPost.appendChild(blogImage);
+  let bottomBorder = document.createElement("div");
+  bottomBorder.setAttribute("class", "border-bottom");
+  let container = document.querySelector(".container");
+  container.appendChild(bottomBorder);
 
   document.querySelector(".container").appendChild(blogPost);
 };
